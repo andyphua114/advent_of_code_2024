2eeae1d8fbd71a44e5f7a8bf2aa221685f04866a
wphua3 <andyphuawc@gmail.com>
2024-12-05 16:03:38 +0800

    day 4 complete

--- day_04/aoc_day4.py
+++ day_04/aoc_day4.py
@@ -49,3 +49,57 @@ for c in combinations:
 
 # check how many are valid "XMAS"
 print(four_letters_list.count("XMAS"))
+
+# PART 2
+
+# get all combinations of the coordinates in the grid
+combinations = [(x, y) for x in range(maxRow+1) for y in range(maxRow+1)]
+
+# we are looking for a pattern where center is "A"
+# topleft is "M", topright is "S", btmleft is "M", btmright is "S"
+# if you unfold it, it will be AMSMS if you search from topleft to topright to btmleft to btmright
+
+
+def return_cross_letter(contents, x, y):
+
+    # direction in topleft, topright, bottomleft, bottomright
+    crossRowDirection = [-1, -1, 1, 1]
+    crossColDirection = [-1, 1, -1, 1]
+
+    maxCol = len(contents[0])-1
+    maxRow = len(contents)-1
+
+    all_cross_words = []
+
+    exit = False
+
+    # store the current letter
+    word = contents[x][y]
+
+    # go through all four directions, and check the word whenever possible (not out-of-bound)
+    for coord in zip(crossRowDirection, crossColDirection):
+
+        if word[0] != "A":
+            word = ""
+            break
+
+        # check whether out-of-bound
+        if coord[0]+x >= 0 and coord[0]+x <= maxRow and coord[1]+y >= 0 and coord[1]+y <= maxCol:
+            word += contents[coord[0]+x][coord[1]+y]
+        else:
+            break
+
+    if len(word) > 0:
+        all_cross_words.append(word)
+
+    return all_cross_words
+
+
+cross_letters_list = []
+
+for c in combinations:
+    cross_letters_list += return_cross_letter(contents, c[0], c[1])
+
+# check how many are valid "X-MAS"
+print(cross_letters_list.count("AMSMS")+cross_letters_list.count("ASSMM") +
+      cross_letters_list.count("ASMSM")+cross_letters_list.count("AMMSS"))
