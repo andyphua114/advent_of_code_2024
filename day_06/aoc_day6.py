3de3adc355afe40ece9fe177d38c4d93bcf4d546
wphua3 <andyphuawc@gmail.com>
2024-12-06 21:26:19 +0800

    day 6 complete

--- day_06/aoc_day6.py
+++ day_06/aoc_day6.py
@@ -21,8 +21,6 @@ for c in combinations:
         position = c
         break
 
-# print(position)
-
 
 def trace_path(direction, position, contents):
 
@@ -43,9 +41,6 @@ def trace_path(direction, position, contents):
     path = {"^": (-1, 0), "v": (1, 0), "<": (0, -1), ">": (0, 1)}
 
     while not oob:
-        # for c in contents:
-        #     print(c)
-        # print("------------------------------")
         for i in range(1, limit):
             # depending on the direction, set the whole straight path as "X" until...
             x = position[0]+path[direction][0]*i
@@ -53,19 +48,14 @@ def trace_path(direction, position, contents):
 
             # never hit obstacle "#"
             if x >= 0 and x <= maxRow and y >= 0 and y <= maxCol and contents[x][y] != "#":
-                # print(x, y)
                 contents[x] = contents[x][:y] + "X" + contents[x][y+1:]
                 path_x.append([(x, y), direction])
-                # current_position = (x-path[direction][0], y-path[direction][1])
-                # position = (x, y)
 
             # hit obstacle "#"
             elif x >= 0 and x <= maxRow and y >= 0 and y <= maxCol and contents[x][y] == "#":
                 hit.append((x, y))
                 # rotate direction after hitting obstacle
-                # print(direction)
                 position = (x-path[direction][0], y-path[direction][1])
-                # print(position)
 
                 if direction == "^":
                     direction = ">"
@@ -75,7 +65,6 @@ def trace_path(direction, position, contents):
                     direction = "<"
                 else:
                     direction = "^"
-                # print(direction)
                 break
 
             # or if go out-of-bound
@@ -90,23 +79,16 @@ def trace_path(direction, position, contents):
 
 path_x, hit = trace_path(direction, position, contents)
 
-# for c in contents:
-#     print(c)
-
 sum = 0
 
 for c in contents:
     sum += c.count("X")
-    # sum += c.count("^")
 
 print(sum)
 
 # PART 2
 
-# the barrel must be placed on the current path marked with "X"
-# if any of the one barrel result in going into a new path, then it won't work
-
-# print(path_x)
+# the obstacle must be placed on the current path marked with "X"
 
 # contents is the mapped out path
 # original is the original empty map
@@ -116,24 +98,22 @@ path_x.append([(original_position), "^"])
 direction = "^"
 
 total = 0
-unknown = 0
-dot = 0
 
 replacement_x = [c[0] for c in path_x]
+
+# get unique list of coordinates to replace "X" with "#"
 replacement_x = list(set(replacement_x))
 
 for idx, c in enumerate(replacement_x):
-    # print("Executing...")
-    # print(c[0])
+
     print(idx)
     position = original_position
     direction = "^"
     new_path_x = []
 
     new_map = original.copy()
+    # put an obstacle
     new_map[c[0]] = new_map[c[0]][:c[1]] + "#" + new_map[c[0]][c[1]+1:]
-    # for m in new_map:
-    #     print(m)
 
     maxCol = len(contents[0])-1
     maxRow = len(contents)-1
@@ -155,41 +135,23 @@ for idx, c in enumerate(replacement_x):
             # depending on the direction, set the whole straight path as "X" until...
             x = position[0]+path[direction][0]*i
             y = position[1]+path[direction][1]*i
-            # print(x)
-            # print(y)
 
             # never hit obstacle "#"
             if x >= 0 and x <= maxRow and y >= 0 and y <= maxCol and new_map[x][y] != "#":
-                if contents[x][y] == "X" and [(x, y), direction] not in new_path_x:
+                if [(x, y), direction] not in new_path_x:
                     new_path_x.append([(x, y), direction])
-                    # print("Continue")
+                # if the same coord + direction happens again, means it is going to go on same loop
                 elif [(x, y), direction] in new_path_x:
-                    # print("SUCCEED")
-                    # print(c[0])
-                    # print("----------------------")
                     total += 1
                     oob = True
-                elif contents[x][y] == ".":
-                    # print((x, y))
-                    # print(direction)
-                    # print([(x, y), direction])
-                    # print(new_path_x)
-                    # print("Got into . path")
-                    dot += 1
-                    oob = True
-                    flag = "fail"
                 else:
-                    print(idx)
-                    unknown += 1
                     oob = True
                     flag = "fail"
 
             # hit obstacle "#"
             elif x >= 0 and x <= maxRow and y >= 0 and y <= maxCol and new_map[x][y] == "#":
 
-                # print("HIT #")
                 position = (x-path[direction][0], y-path[direction][1])
-                # print(position)
 
                 if direction == "^":
                     direction = ">"
@@ -199,18 +161,15 @@ for idx, c in enumerate(replacement_x):
                     direction = "<"
                 else:
                     direction = "^"
-                # print(direction)
                 break
 
             # or if go out-of-bound
             elif x < 0 or x > maxRow or y < 0 or y > maxCol:
                 position = (x-path[direction][0], y-path[direction][1])
-                # print("OOB")
                 oob = True
 
             if oob:
                 break
 
-print(total)
-print(unknown)
-print(dot)
+print("Part 1: {}".format(sum))
+print("Part 2: {}".format(total))
