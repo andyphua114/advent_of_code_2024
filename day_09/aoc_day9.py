da60c79c660a9abd034e84544c26b473751b0346
wphua3 <andyphuawc@gmail.com>
2024-12-09 20:18:25 +0800

    Part 1 working; Part 2 works for examples not input

--- day_09/aoc_day9.py
+++ day_09/aoc_day9.py
@@ -1,119 +1,126 @@
 with open('input.txt') as f:
     contents = f.readlines()
     content = contents[0]
-    contents = [c for c in content]
-
-# print(contents)
+    contents = [int(c) for c in content]
 
 maxLength = len(contents)
 maxId = maxLength//2
-# print(maxId)
 
-raw_sequence = []
-total_odd = 0
-total_even = 0
+file_idx = 0
+space_idx = 0
 
+# PART 1
+
+# get the file block digits only, which are the even placed digit
+
+fileblock_digits = [int(i) for idx, i in enumerate(contents) if idx % 2 == 0]
+reverse_fileblock_digits = fileblock_digits[::-1]
+space_digits = [int(i) for idx, i in enumerate(contents) if idx % 2 != 0]
+
+reverse_value = [i for i in range(maxId, -1, -1)]
+
+final = []
+
+reverse_idx = 0
+
+# there are no zero digit in the even position
 for idx, i in enumerate(contents):
-    if idx % 2 == 0:  # check even, if so, then is block value
-        block_value = idx//2
-        sequence = str(block_value) * int(i)
-        total_even += int(i)
-    else:
-        sequence = "." * int(i)
-        total_odd += int(i)
-    raw_sequence.append(sequence)
-
-new_sequence = [r for r in "".join(raw_sequence) if r != "."]
-reverse_sequence = [r for r in "".join(raw_sequence) if r != "."][::-1]
-
-end = False
-swap_index = -1
-
-# print(new_sequence)
-# print(reverse_sequence)
-
-final_sequence = []
-
-front_pointer = 0
-rear_pointer = 0
-
-total_diff = total_even - total_odd
-
-total_run = 0
-
-for idx, i in enumerate(contents):
-
-    if idx % 2 == 0:  # check even, if so, then is block value
-        block_value = idx//2
-        sequence = new_sequence[front_pointer:front_pointer+int(i)]
-        final_sequence.append(sequence)
-        front_pointer += int(i)
-    else:
-        sequence = reverse_sequence[rear_pointer:rear_pointer+int(i)]
-        final_sequence.append(sequence)
-        rear_pointer += int(i)
-
-    if front_pointer > total_diff:
-        front_pointer += int(contents[idx+1])
-        sequence = new_sequence[front_pointer:front_pointer +
-                                int(contents[idx+1])]
-        final_sequence.append(sequence)
-        rear_pointer += int(contents[idx+1])
-        sequence = reverse_sequence[rear_pointer:rear_pointer +
-                                    int(contents[idx+1])]
-        final_sequence.append(sequence)
+    if idx//2 >= reverse_value[reverse_idx]:
+        final.append(
+            (reverse_value[reverse_idx], reverse_fileblock_digits[reverse_idx]))
         break
+    elif idx % 2 == 0:  # check even, if so, then is block value
+        final.append((idx//2, i))
+    else:
+        # i is the number of space
+        while i >= reverse_fileblock_digits[reverse_idx]:
+            i = i - reverse_fileblock_digits[reverse_idx]
+            final.append(
+                (reverse_value[reverse_idx], reverse_fileblock_digits[reverse_idx]))
+            reverse_fileblock_digits[reverse_idx] = 0
+            reverse_idx += 1
+        if i < reverse_fileblock_digits[reverse_idx] and i != 0:
+            final.append((reverse_value[reverse_idx], i))
+            reverse_fileblock_digits[reverse_idx] -= i
 
+    # print(reverse_fileblock_digits)
 
-total_sequence = [i for sequence in final_sequence for i in sequence]
-# print(total_sequence)
-# space = new_sequence.count(".")
-# tentative_space = space
-# print(space)
-
-# new_sequence_reversed = new_sequence[::-1]
-# count_file = 0
-# count_space = 0
-# for i in new_sequence_reversed:
-#     if i == ".":
-#         count_space += 1
-#         tentative_space -= 1
-#     else:
-#         count_file += 1
-#     if count_file == tentative_space:
-#         break
-
-# new_space = space = tentative_space
-
-# files_sequence = [(f, g)
-#                   for (f, g) in enumerate(new_sequence) if g != "."][::-1]
-# print(files_sequence)
-
-# replacement_sequence = files_sequence[0:new_space]
-# print(replacement_sequence)
-
-# print(new_sequence)
-
-# reverse_idx = -1
-
-# for idx, i in enumerate(new_sequence):
-#     if i == ".":
-#         while new_sequence[reverse_idx] == ".":
-#             reverse_idx -= 1
-#         new_sequence[idx] = new_sequence[reverse_idx]
-#         new_sequence[reverse_idx] = "."
-#         reverse_idx -= 1
-#     if len(new_sequence[idx+1:]) == new_sequence[idx+1:].count("."):
-#         break
-
-# print(new_sequence)
+# print(final)
 
 total = 0
+indice = 0
+cnt = 0
 
-for idx, i in enumerate(total_sequence):
-    if i == ".":
-        break
-    else:
-        total += idx * int(i)
+for a, b in final:
+    cnt = cnt + b
+    while indice < cnt:
+        total += a * indice
+        indice += 1
+
+print(total)
+
+# PART 2
+
+with open('input.txt') as f:
+    contents = f.readlines()
+    content = contents[0]
+    contents = [int(c) for c in content]
+
+start_position = 0
+
+file = []
+
+for idx, c in enumerate(contents):
+    if idx % 2 == 0:  # check even, if so, then is block value
+        file.append((idx//2, c, start_position))
+    start_position += c
+
+reversed_file = file[::-1]
+# print(file)
+
+space_digits = [int(i) for idx, i in enumerate(contents) if idx % 2 != 0]
+
+# print(space_digits)
+space_info = []
+
+for s, f in zip(space_digits, file[:-2]):
+    space_info.append([s, f[1]+f[2]])
+
+# print(space_info)
+
+new_file = []
+
+for idx, f in enumerate(reversed_file[:-1]):
+    restart = False
+    for index, space in enumerate(space_info):
+        if f[1] <= space[0]:
+            # print(f[1])
+            # print(index, space)
+            # print(file[index][1])
+            # print(file[index][2])
+            new_f = (f[0], f[1], space_info[index][1])
+            # print(new_f)
+            # print("_______________________________________")
+            new_file.append(new_f)
+            reversed_file = [r for r in reversed_file if r != f]
+            space_info[index][0] = space_info[index][0] - f[1]
+            space_info[index][1] = space_info[index][1] + f[1]
+            restart = True
+            break
+        if restart:
+            break
+
+
+# print(file)
+
+new_file_number = [i[0] for i in new_file]
+remaining = [i for i in file if i[0] not in new_file_number]
+
+full = new_file + remaining
+
+total = 0
+for f in full:
+    for i in range(f[1]):
+        total += f[0] * (i + f[2])
 
 print(total)
